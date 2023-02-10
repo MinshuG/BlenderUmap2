c42c525e4ffb21150816ab4729f1ac81963c20b9
Minshu Gurjar <minshugurjar@gmail.com>
2023-02-10 08:25:27 +0530

    move stuff

--- make_release.py
+++ make_release.py
@@ -27,7 +27,7 @@ def add_files_to_zip(zip_file: zipfile.ZipFile, pattern, prefix=''):
 try: os.mkdir('release')
 except FileExistsError: pass
 
-for target in ["osx.12-x64", "win-x64", "linux-x64"]:
+for target in ["osx.12-x64", "win-x64", "linux-x64"]: # we can do this because we dpn't need CUE4Parse-Natives
     try:
         for f in glob.glob("./BlenderUmap/bin/Publish/**"):
             os.remove(f)
@@ -36,9 +36,9 @@ for target in ["osx.12-x64", "win-x64", "linux-x64"]:
     code = os.system("dotnet publish BlenderUmap -c Release -r %s --no-self-contained -o \"./BlenderUmap/bin/Publish/\" -p:PublishSingleFile=true -p:DebugType=None -p:DebugSymbols=false -p:IncludeAllContentForSelfExtract=true -p:AssemblyVersion=%s -p:FileVersion=%s"%(target, version, version))
     if code != 0:
         raise Exception(f"dotnet publish failed with code {code}")
-    zipf = zipfile.ZipFile(f'release/BlenderUmap-{target}.zip', 'w', zipfile.ZIP_LZMA, allowZip64=True, compresslevel=9)
+    zipf = zipfile.ZipFile(f'release/BlenderUmap-{version}-{target}.zip', 'w', zipfile.ZIP_LZMA, allowZip64=True, compresslevel=9)
     add_files_to_zip(zipf, "./BlenderUmap/bin/Publish/**", "BlenderUmap/")
-    add_files_to_zip(zipf, "./Addon/*.py", "BlenderUmap/")
+    add_files_to_zip(zipf, "./Importers/Blender/*.py", "BlenderUmap/")
 
     zipf.writestr("BlenderUmap/__version__.py", version_f.getvalue())
     zipf.close()
